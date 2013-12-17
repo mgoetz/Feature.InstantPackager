99aafcf07b06005b158847f1b37b762069e5ec9f
Matt Goetz <matt.goetz@velir.com>
2013-12-16 21:49:23 -0500

    Issue #4: Add gutter icon for instant package content.

---
+++ src/Sitecore.InstantPackager/CustomSitecore/Gutters/InstantPackageGutter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sitecore.Data.Items;
+using Sitecore.SharedSource.InstantPackager.Utils.ItemKeys;
+using Sitecore.SharedSource.InstantPackager.Utils.PackageManager;
+using Sitecore.Shell.Applications.ContentEditor.Gutters;
+
+namespace Sitecore.SharedSource.InstantPackager.CustomSitecore.Gutters
+{
+	class InstantPackageGutter : GutterRenderer
+	{
+		protected override GutterIconDescriptor GetIconDescriptor(Item item)
+		{
+			GutterIconDescriptor gutterIcon = null;
+			InstantPackageManager manager = new InstantPackageManager(new PackageSourceDictionary());
+			IItemKey itemKey = new ItemKey(item);
+			if (manager.Contains(itemKey))
+			{
+				gutterIcon = new GutterIconDescriptor();
+				gutterIcon.Icon = "Core2/32x32/attach.png";
+				gutterIcon.Tooltip = "This item will be downloaded with the next InstantPackage.";
+			}
+			return gutterIcon;
+		}
+	}
+}
